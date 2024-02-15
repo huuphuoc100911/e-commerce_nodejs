0a9da409589682b97d3b3544fbec0842fb62e790
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2024-02-15 15:45:03 +0700

    setup compression

--- src/app.js
+++ src/app.js
@@ -6,18 +6,23 @@ const morgan = require("morgan");
 // Ngăn chặn bên thứ 3 truy cập vào
 const { default: helmet } = require("helmet");
 
+const compression = require("compression");
+
 const app = express();
 
 // Init Middleware
 app.use(morgan("dev"));
 app.use(helmet());
+app.use(compression());
 
 // Init DB
 
 // Init routes
 app.get("/", (req, res, next) => {
+    const strCompress = "Hello";
     return res.status(200).json({
         message: "Welcome",
+        metadata: strCompress.repeat(10000),
     });
 });
 
