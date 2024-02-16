3f2b98fe6f8b4a720ae98861160f693be54feaa5
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2024-02-16 20:11:54 +0700

    config env

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require("./src/app");
 
-const PORT = 3055;
+const PORT = process.env.PORT || 3055;
+console.log(process.env.PORT);
 
 const server = app.listen(PORT, () => {
     console.log(`eCommerce start with port ${PORT}`);
